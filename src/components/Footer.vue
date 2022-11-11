832f18120b7b1a50863e16466eccd53b027ccf71
paoloevan <paoloev1900@gmail.com>
2022-11-11 14:14:32 +0100

    add footer_footer section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -206,7 +206,23 @@ export default {
                 </div>
             </div>
         </div>
+
         <div class="footer_footer">
+            .<div class="container">
+                <button>sign-up now</button>
+
+                <div class="social">
+                    <span>follow us</span>
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-youtube"></i>
+                    <i class="fa-brands fa-pinterest-p"></i>
+                    <i class="fa-solid fa-location-dot"></i>
+                </div>
+            </div>
+
+
+
         </div>
     </footer>
     <!-- / footer -->
