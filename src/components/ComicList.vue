93d1895369b2e7a40e148fdb5c2714fb46827233
paoloevan <paoloev1900@gmail.com>
2022-11-11 16:12:25 +0100

    fix style

--- src/components/ComicList.vue
+++ src/components/ComicList.vue
@@ -24,36 +24,3 @@ export default {
 
     </div>
 </template>
-
-<style lang="scss" scoped>
-.container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    padding: 3rem 0;
-
-    .my_card {
-
-        .card_image {
-            width: 200px;
-            aspect-ratio: 1;
-            padding: 0.5rem;
-
-
-            img {
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-            }
-        }
-
-        .card_title {
-            color: white;
-            text-transform: uppercase;
-            font-size: 0.75rem;
-            padding: 0.5rem;
-
-        }
-    }
-}
-</style>
